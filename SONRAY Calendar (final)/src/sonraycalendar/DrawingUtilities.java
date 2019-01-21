5e9540fdbd253e562e0eb00ba9258f9a16ba45e3
nrudba12 <44201265+nrudba12@users.noreply.github.com>
2019-01-20 19:15:23 -0500

    Add files via upload

---
+++ SONRAY Calendar (final)/src/sonraycalendar/DrawingUtilities.java
@@ -0,0 +1,330 @@
+package sonraycalendar;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
+
+public final class DrawingUtilities { //utility class for drawing stuff
+	//chose to draw images then return smaller versions of those images
+	//instead of just returning path2d (even though paths would've taken
+	//up a lot less lines of codes) because it looked bad quality when
+	//drawn, even when set rendering hints
+	private DrawingUtilities() {}
+	
+	private static Image getUnscaledIcon(Path2D path, Color bgcolor, Color strcolor, int width, int height, int strwidth) {
+		Image img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+		Graphics2D g2d = (Graphics2D) img.getGraphics();
+		
+		g2d.setColor(bgcolor);
+		g2d.fillRect(0, 0, width, height);
+		g2d.setColor(strcolor);
+		g2d.setStroke(new BasicStroke(strwidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+		g2d.draw(path);
+		
+		g2d.dispose();
+		
+		return img;
+	}
+	
+	public static String getFittedString(FontMetrics fontMetrics, String str, int space) {
+		int len = fontMetrics.stringWidth(str);
+		int oldstrlen = str.length();
+		int newstrlen = oldstrlen;
+		while (len > space - 10) len = fontMetrics.stringWidth(str.substring(0, (--newstrlen)) + "...");
+		if (newstrlen != oldstrlen) str = str.substring(0, newstrlen) + "...";
+		return str;
+	}
+	
+	public static Image clock(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int strwidth = unscaledSize / 16;
+		
+		int space = unscaledSize / 16;
+		int centre = unscaledSize / 2;
+		int radius = (unscaledSize - 2*space - 2*strwidth) / 2;
+		
+		Path2D clock = new Path2D.Float();
+		
+		clock.append(new Ellipse2D.Float(strwidth / 2, strwidth / 2, unscaledSize - strwidth, unscaledSize - strwidth), false);
+		clock.append(new Ellipse2D.Float(space + strwidth, space + strwidth, radius * 2, radius * 2), false);
+		
+		clock.moveTo(centre, centre - radius/2);
+		clock.lineTo(centre, centre);
+		clock.lineTo(centre + radius/2, centre);
+		
+		for (double i = 0; i <= Math.PI / 2; i += Math.PI / 6) {
+			int x1 = (int) (radius * Math.cos(i));
+			int y1 = (int) (radius * Math.sin(i));
+			int x2 = (int) ((radius - (space + strwidth/2)) * Math.cos(i));
+			int y2 = (int) ((radius - (space + strwidth/2)) * Math.sin(i));
+			
+			clock.moveTo(centre + x1, centre - y1);
+			clock.lineTo(centre + x2, centre - y2);
+			clock.moveTo(centre - x1, centre - y1);
+			clock.lineTo(centre - x2, centre - y2);
+			clock.moveTo(centre + x1, centre + y1);
+			clock.lineTo(centre + x2, centre + y2);
+			clock.moveTo(centre - x1, centre + y1);
+			clock.lineTo(centre - x2, centre + y2);
+		}
+		
+		Image unscaledClock = getUnscaledIcon(clock, bgcolor, strcolor, unscaledSize, unscaledSize, strwidth);
+		
+		return unscaledClock.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image notes(int width, Color bgcolor, Color strcolor) {
+		int unscaledWidth = 4 * width + 32 - (4 * width) % 32;
+		int unscaledHeight = unscaledWidth * 9 / 16;
+		int halfstrwidth = unscaledHeight / 8;
+		
+		Path2D notes = new Path2D.Float();
+		
+		notes.moveTo(halfstrwidth, halfstrwidth);
+		notes.lineTo(unscaledWidth - halfstrwidth, halfstrwidth);
+		notes.moveTo(halfstrwidth, unscaledHeight / 2);
+		notes.lineTo(unscaledWidth - halfstrwidth, unscaledHeight / 2);
+		notes.moveTo(halfstrwidth, unscaledHeight - halfstrwidth);
+		notes.lineTo(unscaledWidth / 2, unscaledHeight - halfstrwidth);
+		
+		Image unscaledNotes = getUnscaledIcon(notes, bgcolor, strcolor, unscaledWidth, unscaledHeight, 2 * halfstrwidth);
+		
+		return unscaledNotes.getScaledInstance(width, width * 9 / 16, Image.SCALE_AREA_AVERAGING);
+		
+	}
+	
+	public static Image pencil(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int halfstrwidth = unscaledSize / 32;
+		
+		int spacing = unscaledSize / 8;
+		
+		int topLeftX = unscaledSize * 3 / 4;
+		int topLeftY = halfstrwidth;
+		int topRightX = unscaledSize - halfstrwidth;
+		int topRightY = unscaledSize * 1 / 4;
+		
+		int bottomRightX = unscaledSize * 1 / 4 + spacing;
+		int bottomRightY = unscaledSize - spacing;
+		int bottomLeftX = halfstrwidth + spacing;
+		int bottomLeftY = unscaledSize - bottomRightX;
+		
+		Path2D pencil = new Path2D.Float();
+		
+		pencil.moveTo(bottomRightX, bottomRightY);
+		pencil.lineTo(halfstrwidth, unscaledSize - halfstrwidth);
+		pencil.lineTo(bottomLeftX, bottomLeftY);
+		
+		pencil.moveTo(bottomLeftX, bottomLeftY);
+		pencil.lineTo(bottomRightX, bottomRightY);
+		
+		pencil.moveTo(topLeftX - spacing, topLeftY + spacing);
+		pencil.lineTo(topRightX - spacing, topRightY + spacing);
+		pencil.lineTo(topRightX, topRightY);
+		pencil.lineTo(topLeftX, topLeftY);
+		pencil.lineTo(bottomLeftX, bottomLeftY);
+		
+		for (int i = 1; i < 4; i++) {
+			pencil.moveTo(bottomLeftX + (bottomRightX - bottomLeftX) * i / 3, bottomLeftY + (bottomRightX - bottomLeftX) * i / 3);
+			pencil.lineTo(topLeftX - spacing + (topRightX - topLeftX) * i / 3, topLeftY + spacing + (topRightY - topLeftY) * i / 3);
+		}
+		
+		Image unscaledPencil = getUnscaledIcon(pencil, bgcolor, strcolor, unscaledSize, unscaledSize, 2 * halfstrwidth);
+		
+		return unscaledPencil.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image search(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int halfstrwidth = unscaledSize / 32;
+		
+		int space = unscaledSize * 3 / 32;
+		int radius = unscaledSize * 3 / 8;
+		int centre = halfstrwidth + radius;
+		
+		int mid = (int) (radius / Math.sqrt(2));
+		int end = unscaledSize - halfstrwidth;
+		
+		int x1 = (int) (radius * Math.cos(Math.PI * 7 / 32));
+		int y1 = (int) (radius * Math.sin(Math.PI * 7 / 32));
+		int x2 = (int) (radius * Math.cos(Math.PI * 9 / 32));
+		int y2 = (int) (radius * Math.sin(Math.PI * 9 / 32));
+		
+		Path2D search = new Path2D.Float();
+		
+		search.append(new Ellipse2D.Float(halfstrwidth, halfstrwidth, 2 * radius, 2 * radius), false);
+		search.append(new Ellipse2D.Float(halfstrwidth + space, halfstrwidth + space, 2 * (radius - space), 2 * (radius - space)), false);
+		
+		search.moveTo(centre + x1, centre + y1);
+		search.lineTo(end, end - 2 * (mid - y1));
+		search.lineTo(end - 2 * (mid - x2), end);
+		search.lineTo(centre + x2, centre + y2);
+		
+		Image unscaledSearch = getUnscaledIcon(search, bgcolor, strcolor, unscaledSize, unscaledSize, 2 * halfstrwidth);
+		
+		return unscaledSearch.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image plus(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int halfstrwidth = unscaledSize / 8;
+		
+		int centre = unscaledSize / 2;
+		
+		Path2D plus = new Path2D.Float();
+		
+		plus.moveTo(centre, halfstrwidth);
+		plus.lineTo(centre, unscaledSize - halfstrwidth);
+		plus.moveTo(halfstrwidth, centre);
+		plus.lineTo(unscaledSize - halfstrwidth, centre);
+		
+		Image unscaledAdd = getUnscaledIcon(plus, bgcolor, strcolor, unscaledSize, unscaledSize, 2 * halfstrwidth);
+		
+		return unscaledAdd.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image leftArrow(int width, Color bgcolor, Color strcolor) {
+		int unscaledWidth = 4 * width + 72 - width % 18;
+		int unscaledHeight = unscaledWidth * 16 / 9;
+		int halfstrwidth = unscaledHeight / 8;
+		
+		Path2D leftArrow = new Path2D.Float();
+		
+		leftArrow.moveTo(unscaledWidth - halfstrwidth, halfstrwidth);
+		leftArrow.lineTo(halfstrwidth, unscaledHeight / 2);
+		leftArrow.lineTo(unscaledWidth - halfstrwidth, unscaledHeight - halfstrwidth);
+		
+		Image unscaledLeftArrow = getUnscaledIcon(leftArrow, bgcolor, strcolor, unscaledWidth, unscaledHeight, 2 * halfstrwidth);
+		
+		return unscaledLeftArrow.getScaledInstance(width, width * 16 / 9, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image rightArrow(int width, Color bgcolor, Color strcolor) {
+		int unscaledWidth = 4 * width + 72 - (4 * width) % 72;
+		int unscaledHeight = unscaledWidth * 16 / 9;
+		int halfstrwidth = unscaledHeight / 8;
+		
+		Path2D rightArrow = new Path2D.Float();
+		
+		rightArrow.moveTo(halfstrwidth, halfstrwidth);
+		rightArrow.lineTo(unscaledWidth - halfstrwidth, unscaledHeight / 2);
+		rightArrow.lineTo(halfstrwidth, unscaledHeight - halfstrwidth);
+		
+		Image unscaledLeftArrow = getUnscaledIcon(rightArrow, bgcolor, strcolor, unscaledWidth, unscaledHeight, 2 * halfstrwidth);
+		
+		return unscaledLeftArrow.getScaledInstance(width, width * 16 / 9, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image garbage(int width, Color bgcolor, Color strcolor) {
+		int unscaledWidth = 4 * width + 96 - (4 * width) % 96;
+		int unscaledHeight = unscaledWidth * 4 / 3;
+		int halfstrwidth = unscaledHeight / 32;
+		
+		int right = unscaledWidth - halfstrwidth;
+		int bottom = unscaledHeight - halfstrwidth;
+		
+		Path2D garbage = new Path2D.Float();
+		
+		garbage.moveTo(halfstrwidth, halfstrwidth);
+		garbage.lineTo(right, halfstrwidth);
+		garbage.lineTo(right, bottom);
+		garbage.lineTo(halfstrwidth, bottom);
+		garbage.closePath();
+		
+		int top = 5 * halfstrwidth;
+		
+		garbage.moveTo(halfstrwidth, top);
+		garbage.lineTo(right, top);
+		
+		for (int i = 1; i <= 3; i++) {
+			int xpos = unscaledWidth * i / 4;
+			
+			garbage.moveTo(xpos, top + unscaledHeight / 8);
+			garbage.lineTo(xpos, bottom - unscaledHeight / 8);
+		}
+		
+		Image unscaledAdd = getUnscaledIcon(garbage, bgcolor, strcolor, unscaledWidth, unscaledHeight, 2 * halfstrwidth);
+		
+		return unscaledAdd.getScaledInstance(width, width * 4 / 3, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image check(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int halfstrwidth = unscaledSize / 32;
+		
+		int centre = unscaledSize / 2;
+		int space = unscaledSize / 16;
+		int radius = (unscaledSize - 2 * halfstrwidth) / 4;
+		
+		int x1 = (int) (radius * Math.cos(Math.PI / 12));
+		int y1 = (int) (radius * Math.sin(Math.PI / 12));
+		int x2 = (int) (radius * Math.cos(Math.PI * 5 / 12));
+		int y2 = (int) (radius * Math.sin(Math.PI * 5 / 12));
+		
+		int slope = -(y2 - y1) / (x2 - x1);
+		int lastx = centre + x1;
+		int lasty = centre - space / 2 - y1;
+		
+		Path2D check = new Path2D.Float();
+		
+		check.append(new Ellipse2D.Float(halfstrwidth, halfstrwidth, unscaledSize - 2 * halfstrwidth, unscaledSize - 2 * halfstrwidth), false);
+		
+		check.moveTo(centre - x1, centre - space / 2 + y1);
+		check.lineTo(centre - x2, centre - space / 2 + y2);
+		check.lineTo(lastx, lasty);
+		
+		int currentx;
+		check.lineTo(currentx = centre + x1 - space, lasty += (lastx - currentx) * -1 / slope);
+		lastx = currentx;
+		check.lineTo(currentx = centre - x2, lasty += (lastx - currentx) * slope);
+		lastx = currentx;
+		check.lineTo(currentx = centre - x1 + space, lasty += (lastx - currentx) * -1 / slope);
+		check.closePath();
+		
+		Image unscaledAdd = getUnscaledIcon(check, bgcolor, strcolor, unscaledSize, unscaledSize, 2 * halfstrwidth);
+		
+		return unscaledAdd.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image pin(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 64 - (4 * size) % 64;
+		int halfstrwidth = unscaledSize / 16;
+		
+		Image location = new BufferedImage(unscaledSize, unscaledSize, BufferedImage.TYPE_INT_RGB);
+		
+		Graphics2D g2d = (Graphics2D) location.getGraphics();
+		
+		g2d.setColor(bgcolor);
+		g2d.fillRect(0, 0, unscaledSize, unscaledSize);
+		g2d.setColor(strcolor);
+		g2d.setStroke(new BasicStroke(2 * halfstrwidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+		
+		g2d.drawOval(unscaledSize / 4, halfstrwidth, unscaledSize / 2, unscaledSize / 2);
+		g2d.drawArc(unscaledSize * 3 / 8, halfstrwidth + unscaledSize / 8, unscaledSize / 4, unscaledSize / 4, 90, 90);
+		g2d.drawLine(unscaledSize / 2, unscaledSize / 2 + halfstrwidth, unscaledSize / 2, unscaledSize - halfstrwidth);
+		
+		return location.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+	
+	public static Image ex(int size, Color bgcolor, Color strcolor) {
+		int unscaledSize = 4 * size + 32 - (4 * size) % 32;
+		int halfstrwidth = unscaledSize / 8;
+		
+		Path2D ex = new Path2D.Float();
+		
+		ex.moveTo(halfstrwidth, halfstrwidth);
+		ex.lineTo(unscaledSize - halfstrwidth, unscaledSize - halfstrwidth);
+		ex.moveTo(halfstrwidth, unscaledSize - halfstrwidth);
+		ex.lineTo(unscaledSize - halfstrwidth, halfstrwidth);
+		
+		Image unscaledEx = getUnscaledIcon(ex, bgcolor, strcolor, unscaledSize, unscaledSize, 2 * halfstrwidth);
+		
+		return unscaledEx.getScaledInstance(size, size, Image.SCALE_AREA_AVERAGING);
+	}
+}
---
+++ SONRAY Calendar (final)/src/sonraycalendar/EventPanel.java
@@ -0,0 +1,354 @@
+package sonraycalendar;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+
+@SuppressWarnings("serial")
+public class EventPanel extends JPanel {
+	public static final String defaultTitle = "Add title";
+	private static final int WIDTH = 200;
+	
+	private final EventData event;
+	
+	private final EventButton edit, update, delete, allDay, setTime;
+	private final JTextField time, date, title;
+	private final JTextArea notes;
+	private final JLabel displayDate, displayTime;
+	
+	private boolean editMode;
+	private int regHeight, editorHeight;
+	
+	public EventPanel(EventData event) {
+		this.event = event;
+		regHeight = 140;
+		editorHeight = 200;
+		
+		edit = new EventButton();
+		delete = new EventButton();
+		update = new EventButton();
+		allDay = new EventButton();
+		setTime = new EventButton();
+		
+		time = new JTextField();
+		date = new JTextField();
+		notes = new JTextArea();
+		title = new JTextField();
+		
+		displayDate = new JLabel();
+		displayTime = new JLabel();
+		
+		editMode = false;
+	}
+	
+	public void init() {
+		setLayout(null); //no layout manager because want to use absolute position of buttons,
+		//text boxes, etc. (had problem earlier where FlowLayout caused text boxes to be placed
+		//side by side even after calling setLocation()
+		setSize(WIDTH, regHeight);
+		setBackground(Color.WHITE);
+		initButtons();
+		initTextFields();
+		
+		addMouseListener(new MouseAdapter() {}); //so that hover or click on this panel
+		//won't affect panels behind it
+		
+		displayDate.setBounds(46, 61, WIDTH - 47, 15);
+		displayTime.setBounds(46, 74, WIDTH - 47, 13);
+		displayDate.setFont(new Font("Arial", Font.PLAIN, 10));
+		displayTime.setFont(new Font("Arial", Font.PLAIN, 8));
+		displayDate.setForeground(Color.BLACK);
+		displayTime.setForeground(Color.GRAY);
+		
+		for (JComponent comp : new JComponent[] {edit, update, delete, allDay, setTime,
+				time, date, notes, title, displayDate, displayTime}) add(comp);
+		
+		setEditMode(false);
+		
+		//makes sure event panel is always at front of screen; since event panel is created
+		//in almost every other class, easier to do here instead of relegating to outside
+		Manager.getInstance().add(this);
+		Manager.getInstance().getContentPane().setComponentZOrder(this, 0);
+		
+		title.grabFocus(); //initializes title and notes
+		notes.grabFocus();
+		grabFocus();
+	}
+	
+	private void initButtons() {
+		edit.setBounds(175, regHeight - 25, 15, 15);
+		update.setBounds(175, editorHeight - 25, 15, 15);
+		delete.setBounds(152, regHeight - 25, 15, 15);
+		//width is much wider than circles representing buttons since better user experience (more info
+		//in github repo)
+		allDay.setBounds(11, 63, WIDTH - 22, 15);
+		setTime.setBounds(11, 83, WIDTH - 22, 15);
+		
+		edit.init(() -> setEditMode(true));
+		update.init(() -> {
+			if (updateEventData()) {
+				setEditMode(false);
+				if (!EventData.events.contains(event)) EventData.events.add(event);
+				Manager manager = Manager.getInstance();
+				//reset calendarPanel, sidePanel, and searchPanel since event updated
+				String selectedDay = manager.calendarPanel.getSelectedDay();
+				manager.sidePanel.setEvents(selectedDay);
+				if (manager.searchPanel != null) manager.searchPanel.reset();
+				else {
+					int[] month = manager.calendarPanel.getMonth();
+					manager.calendarPanel.setMonth(month[0], month[1]);
+				}
+				manager.repaint();
+			}
+		});
+		
+		delete.init(() -> {
+			EventData.events.remove(event);
+			Manager manager = Manager.getInstance();
+			manager.remove(manager.eventPanel);
+			//reset calendarPanel, sidePanel, and searchPanel since event deleted
+			String selectedDay = manager.calendarPanel.getSelectedDay();
+			manager.sidePanel.setEvents(selectedDay);
+			if (manager.searchPanel != null) manager.searchPanel.reset();
+			else {
+				int[] month = manager.calendarPanel.getMonth();
+				manager.calendarPanel.setMonth(month[0], month[1]);
+			}
+			manager.repaint();
+		});
+		
+		allDay.init(() -> {
+			if (!editMode) return;
+			
+			allDay.pressed = true;
+			setTime.pressed = false;
+			setTime.setSize(WIDTH - 22, 15); //bounds reset when text box can no longer be pressed
+			time.setEnabled(false);
+			time.setVisible(false);
+		});
+		
+		setTime.init(() -> {
+			if (!editMode) return;
+			
+			setTime.pressed = true;
+			allDay.pressed = false;
+			setTime.setSize(70, 15); //bounds reset so doesn't get in way of time text box
+			time.setText("12:00 AM - 12:00 PM");
+			time.setEnabled(true);
+			time.setVisible(true);
+		});
+		
+		allDay.pressed = "".equals(event.getTime());
+		setTime.pressed = !allDay.pressed;
+		if (setTime.pressed) setTime.setSize(70, 15);
+	}
+	
+	private void initTextFields() {
+		class TextLimit extends KeyAdapter { //limits num of characters that can be typed in text component
+			private JTextComponent textComponent;
+			private int limit;
+			
+			TextLimit(JTextComponent textComponent, int limit) {
+				this.textComponent = textComponent;
+				this.limit = limit;
+			}
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (textComponent.getText().length() >= limit) e.consume();
+			}
+		}
+		
+		for (JTextComponent textComponent : new JTextComponent[] {time, date, title, notes}) {
+			textComponent.setBorder(null);
+			textComponent.setOpaque(false);
+		}
+		
+		time.setFont(new Font("Arial", Font.PLAIN, 10));
+		date.setFont(new Font("Arial", Font.PLAIN, 10));
+		time.setBounds(82, 83, 105, 15);
+		date.setBounds(42, 109, 145, 15);
+		title.setBounds(10, 24, WIDTH - 20, 20);
+		
+		time.setText(event.getDisplayableTime());
+		
+		try { //reformats date to readable format (MM / dd / yyyy)
+			Date dateob = new SimpleDateFormat("yyyyMMdd").parse(event.getDate());
+			date.setText(new SimpleDateFormat("MM / dd / yyyy").format(dateob));
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		
+		time.addKeyListener(new TextLimit(time, 19));
+		date.addKeyListener(new TextLimit(date, 14));
+		title.addKeyListener(new TextLimit(title, 25));
+		notes.addKeyListener(new TextLimit(notes, 80));
+		
+		//when only whitespace as title or notes, when clicked: text field empty, when not clicked: text field
+		//shows "Add title" or "Add notes" respectively
+		title.setForeground(getBackground());
+		title.addFocusListener(new FocusListener() { 
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (defaultTitle.equals(title.getText()) && editMode) {
+					title.setFont(new Font("Arial", Font.BOLD, 14));
+					title.setText("");
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if ("".equals(title.getText().replace(" ", ""))) {
+					title.setFont(new Font("Arial", Font.BOLD + Font.ITALIC, 14));
+					title.setText(defaultTitle);
+				}
+			}
+		});
+		
+		notes.setRows(3);
+		notes.setLineWrap(true);
+		notes.setWrapStyleWord(true);
+		notes.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if ("Add notes".equals(notes.getText()) && editMode) {
+					notes.setFont(new Font("Arial", Font.PLAIN, 10));
+					notes.setText("");
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if ("".equals(notes.getText().replace(" ", ""))) {
+					notes.setFont(new Font("Arial", Font.ITALIC, 10));
+					notes.setText("Add notes");
+				}
+			}
+		});
+	}
+	
+	private void setEditMode(boolean b) {
+		displayDate.setVisible(!b);
+		displayTime.setVisible(!b);
+		date.setVisible(b);
+		title.setEditable(b);
+		notes.setEditable(b);
+		
+		time.setEnabled(setTime.pressed && b);
+		time.setVisible(setTime.pressed && b);
+		
+		if (b) {		
+			notes.setBounds(45, 130, 143, 43);
+			delete.setBounds(152, editorHeight - 25, 15, 15);
+			setSize(WIDTH, editorHeight);
+		} else {
+			try {
+				Date strdate = new SimpleDateFormat("yyyyMMdd").parse(event.getDate());
+				displayDate.setText(new SimpleDateFormat("EEEE, MMMM d, yyyy").format(strdate));
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+			
+			displayTime.setText("".equals(event.getTime()) ? "all day" : event.getDisplayableTime());
+			
+			try { //changes size depending on number of rows in notes
+				int height = notes.modelToView(notes.getDocument().getEndPosition().getOffset() - 1).y;
+				int lines = height / notes.getFont().getSize() + 1;
+				regHeight = 115 + 15 * (lines);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {} //when first called, throws NullPointerException
+			
+			notes.setBounds(45, 96, 143, 15 * (regHeight - 110));
+			delete.setBounds(152, regHeight - 25, 15, 15);
+			edit.setLocation(175, regHeight - 25);
+			setSize(WIDTH, regHeight);
+		}
+		
+		editMode = b;
+	}
+	
+	private boolean updateEventData() {
+		String title = defaultTitle.equals(this.title.getText()) ? "" : this.title.getText();
+		String notes = "Add notes".equals(this.notes.getText()) ? "" : this.notes.getText();
+		String time = allDay.pressed ? "" : this.time.getText();
+		
+		return event.update(title, date.getText(), time, notes);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+		
+		g2d.setColor(event.getColor(true));
+		g2d.fillRect(0, 0, WIDTH, 50);
+		
+		if (editMode) drawEditMode(g2d);
+		else drawRegularMode(g2d);
+		
+		g2d.setColor(Color.BLACK);
+		g2d.drawRect(0, 0, WIDTH - 1, getHeight() - 1);
+	}
+	
+	private void drawRegularMode(Graphics2D g2d) {
+		Color bgcolor = getBackground();
+		
+		g2d.drawImage(DrawingUtilities.clock(26, bgcolor, Color.GRAY), 10, 60, null);
+		g2d.drawImage(DrawingUtilities.notes(26, bgcolor, Color.GRAY), 10, 100, null);
+		
+		Color pencilColor = edit.hovering() ? Color.RED : Color.GRAY;
+		Color garbageColor = delete.hovering() ? Color.RED : Color.GRAY;
+		g2d.drawImage(DrawingUtilities.pencil(15, bgcolor, pencilColor), 175, regHeight - 25, null);
+		g2d.drawImage(DrawingUtilities.garbage(11, bgcolor, garbageColor), 154, regHeight - 25, null);
+	}
+	
+	private void drawEditMode(Graphics2D g2d) {
+		Color bgcolor = getBackground();
+		
+		String[] labels = {"all day", "set time"};
+		EventButton[] buttons = {allDay, setTime};
+		for (int i = 0; i < 2; i++) { //only does twice but didn't want to write twice
+			Color filling;
+			if (buttons[i].pressed) filling = Color.RED;
+			else if (buttons[i].hovering()) filling = new Color(255, 153, 153);
+			else filling = bgcolor;
+			
+			g2d.setColor(Color.BLACK);
+			g2d.drawOval(11, 63 + i * 20, 15, 15);
+			g2d.setColor(filling);
+			g2d.fillOval(14, 66 + i * 20, 10, 10);
+			
+			g2d.setColor(Color.BLACK);
+			g2d.setFont(new Font("Arial", Font.PLAIN, 10));
+			g2d.drawString(labels[i], 38, 74 + i * 20);
+		}
+		
+		g2d.drawString("Date:", 10, 120);
+		g2d.drawImage(DrawingUtilities.notes(20, bgcolor, Color.GRAY), 10, 135, null);
+		
+		Color checkColor = update.hovering() ? Color.RED : Color.GRAY;
+		Color garbageColor = delete.hovering() ? Color.RED : Color.GRAY;
+		g2d.drawImage(DrawingUtilities.check(15, bgcolor, checkColor), 175, editorHeight - 25, null);
+		g2d.drawImage(DrawingUtilities.garbage(11, bgcolor, garbageColor), 154, editorHeight - 25, null);
+	}
+}
